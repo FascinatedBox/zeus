b6141c34ae66c44f9462fff790ca293740778390
FascinatedBox <FascinatedBox@protonmail.com>
2025-02-12 17:30:38 -0800

    Be more accepting of values in key->value pairs.
    I assume this is safe enough for the size and scope of this application.

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -2,6 +2,8 @@
 
 #include "utils.h"
 
+bool isValidPulseDeviceValue(QString name);
+
 bool isValidPropertyKV(QString key, QString value) {
   if (isValidPulseDeviceName(key) == false)
     return false;
@@ -9,7 +11,7 @@ bool isValidPropertyKV(QString key, QString value) {
   if (value.size() == 0)
     return true;
 
-  return isValidPulseDeviceName(value);
+  return isValidPulseDeviceValue(value);
 }
 
 bool isValidPulseDeviceName(QString name) {
@@ -23,6 +25,29 @@ bool isValidPulseDeviceName(QString name) {
     char ch = *raw;
 
     if (isalnum(ch) == false && ch != '-' && ch != '_' && ch != '.') {
+      result = false;
+      break;
+    }
+
+    raw++;
+  }
+
+  return result;
+}
+
+bool isValidPulseDeviceValue(QString name) {
+  if (name.size() == 0 || name.size() > 100)
+    return false;
+
+  const char *raw = name.toLocal8Bit().data();
+  bool result = true;
+
+  while (*raw) {
+    char ch = *raw;
+
+    // I don't expect untrusted input to flow through here, so only check for
+    // what I think would break stuff.
+    if (ch == '"' || ch == '\\' || ch == '\'') {
       result = false;
       break;
     }
